cf3257ff776fc05f86c1f0cdb803dabd805f0cd8
Matheus <matheuschmidt.b@gmail.com>
2025-04-08 16:48:58 -0300

    feat: add InvoiceService implementation

---
+++ go-api/internal/service/invoice_service.go
@@ -0,0 +1,90 @@
+package service
+
+import (
+	"github.com/bantheus/gateway-pagamento/api-go/internal/domain"
+	"github.com/bantheus/gateway-pagamento/api-go/internal/dto"
+)
+
+type InvoiceService struct {
+	invoiceRepository domain.InvoiceRepository
+	accounteService   AccountService
+}
+
+func NewInvoiceService(invoiceService domain.InvoiceRepository, accountService AccountService) *InvoiceService {
+	return &InvoiceService{
+		invoiceRepository: invoiceService,
+		accounteService:   accountService,
+	}
+}
+
+func (s *InvoiceService) Create(input *dto.CreateInvoiceInput) (*dto.InvoiceOutput, error) {
+	accountOutput, err := s.accounteService.FindByAPIKey(input.APIKey)
+	if err != nil {
+		return nil, err
+	}
+
+	invoice, err := dto.ToInvoice(input, accountOutput.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := invoice.Process(); err != nil {
+		return nil, err
+	}
+
+	if invoice.Status == domain.StatusApproved {
+		_, err = s.accounteService.UpdateBalance(input.APIKey, invoice.Amount)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if err := s.invoiceRepository.Save(invoice); err != nil {
+		return nil, err
+	}
+
+	output := dto.FromInvoice(invoice)
+	return &output, nil
+}
+
+func (s *InvoiceService) GetByID(id, apiKey string) (*dto.InvoiceOutput, error) {
+	invoice, err := s.invoiceRepository.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	accountOutput, err := s.accounteService.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	if invoice.AccountID != accountOutput.ID {
+		return nil, domain.ErrUnauthorizedAccess
+	}
+
+	output := dto.FromInvoice(invoice)
+	return &output, nil
+}
+
+func (s *InvoiceService) ListByAccount(accountId string) ([]*dto.InvoiceOutput, error) {
+	invoices, err := s.invoiceRepository.FindByAccountID(accountId)
+	if err != nil {
+		return nil, err
+	}
+
+	output := make([]*dto.InvoiceOutput, len(invoices))
+	for i, invoice := range invoices {
+		invoiceOutput := dto.FromInvoice(invoice)
+		output[i] = &invoiceOutput
+	}
+	return output, nil
+}
+
+func (s *InvoiceService) ListByAccountAPIKey(apiKey string) ([]*dto.InvoiceOutput, error) {
+	accountOutput, err := s.accounteService.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.ListByAccount(accountOutput.ID)
+}
