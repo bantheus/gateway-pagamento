0e105bb73e3d18e7b6c0aba92c80080e95887b48
Matheus <matheuschmidt.b@gmail.com>
2025-04-08 16:55:55 -0300

    refactor: replace pointer usage

--- go-api/internal/service/invoice_service.go
+++ go-api/internal/service/invoice_service.go
@@ -17,7 +17,7 @@ func NewInvoiceService(invoiceService domain.InvoiceRepository, accountService A
 	}
 }
 
-func (s *InvoiceService) Create(input *dto.CreateInvoiceInput) (*dto.InvoiceOutput, error) {
+func (s *InvoiceService) Create(input dto.CreateInvoiceInput) (*dto.InvoiceOutput, error) {
 	accountOutput, err := s.accounteService.FindByAPIKey(input.APIKey)
 	if err != nil {
 		return nil, err
